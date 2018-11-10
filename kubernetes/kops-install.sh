3f2301224dbb03a32b392e8b6cad8904bf9526da
fdupoux <fdupoux@users.sourceforge.net>
2018-11-10 18:38:47 +0000

    Code cleanup and increase volume sizes

--- kubernetes/kops-install.sh
+++ kubernetes/kops-install.sh
@@ -2,8 +2,7 @@
 
 # Define environment variables
 export KOPS_STATE_STORE=$(< ~/SECRETS/aws-kops-cfg-bucket.txt)
-export KOPS_ROUTE53_ZONEID=$(< ~/SECRETS/aws-kops-cfg-zoneid.txt)
-export KOPS_ROUTE53_DOMAIN=$(< ~/SECRETS/aws-kops-cfg-domain.txt)
+export KOPS_CLUSTER_NAME=$(< ~/SECRETS/aws-kops-cfg-cluster-name.txt)
 export KOPS_TRUSTED_IPS=$(< ~/SECRETS/aws-kops-cfg-trusted-ips.txt)
 export AWS_ACCESS_KEY_ID=$(< ~/SECRETS/aws-kops-key-access.txt)
 export AWS_SECRET_ACCESS_KEY=$(< ~/SECRETS/aws-kops-key-secret.txt)
@@ -11,7 +10,9 @@ export AWS_SECRET_ACCESS_KEY=$(< ~/SECRETS/aws-kops-key-secret.txt)
 # Parameters
 export AWS_REGION='eu-west-1'
 export AWS_VPC_CIDR='10.77.0.0/16'
-export KOPS_CLUSTER_NAME="${KOPS_ROUTE53_DOMAIN}"
+export KOPS_EBS_VOL_SIZE="16"
+export KOPS_EC2_TYPE_MASTERS="t2.micro"
+export KOPS_EC2_TYPE_SLAVES="t2.micro"
 
 # Print variables
 echo "AWS_REGION='${AWS_REGION}'"
@@ -27,9 +28,9 @@ KOPS_DRY_RUN="--dry-run --output yaml"
               --name=${KOPS_CLUSTER_NAME} \
               --dns-zone=${KOPS_CLUSTER_NAME} \
               --state=${KOPS_STATE_STORE} \
-              --master-count=1 --master-zones=${AWS_REGION}a --master-size=t2.micro --master-volume-size 8 \
-              --node-count=2 --zones ${AWS_REGION}a,${AWS_REGION}b --node-size=t2.micro --node-volume-size 10 \
+              --master-count=1 --master-zones=${AWS_REGION}a --master-size=${KOPS_EC2_TYPE_MASTERS} --master-volume-size ${KOPS_EBS_VOL_SIZE} \
+              --node-count=2 --zones ${AWS_REGION}a,${AWS_REGION}b --node-size=${KOPS_EC2_TYPE_SLAVES} --node-volume-size ${KOPS_EBS_VOL_SIZE} \
               --network-cidr "${AWS_VPC_CIDR}" \
-              --admin-access "${AWS_VPC_CIDR},${KOPS_TRUSTED_IPS}" \
+              --admin-access "${KOPS_TRUSTED_IPS}" \
               --ssh-access "${KOPS_TRUSTED_IPS}" \
               --yes
