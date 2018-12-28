4e8d03b7b2e69a05dc3e77d2daf4e6579f640ff6
fdupoux <fdupoux@users.sourceforge.net>
2018-12-28 17:15:03 +0000

    Support for importing GnuPG public keys

--- docker/docker-rubackup/files/start.sh
+++ docker/docker-rubackup/files/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ -n "${RUBACKUP_IMPORT_GPG_PUBKEY}" ]
+then
+    /usr/bin/wget ${RUBACKUP_IMPORT_GPG_PUBKEY} -O /var/tmp/gnupg-pubkey.txt
+    /usr/bin/gpg --import /var/tmp/gnupg-pubkey.txt
+    /usr/bin/gpg --list-keys --fingerprint --with-colons | sed -E -n -e 's/^fpr:::::::::([0-9A-F]+):$/\1:6:/p' | gpg --import-ownertrust
+fi
+
 echo "Starting rubackup.rb"
-exec /opt/rubackup/rubackup.rb --logfile /var/log/rubackup/rubackup.dbg --loglevel 4 --outlevel 3
+exec /opt/rubackup/rubackup.rb --logfile /var/log/rubackup/rubackup.log --loglevel 4 --outlevel 3
